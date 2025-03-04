8e080c7d1ae6815320c3887a54258dbb8b9f87b7
wesleynjerenga <wesleynjerenga9@gmail.com>
2025-03-04 20:35:21 +0300

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+let num1 = 31;
+let num2 = 2;
 
-// Write your code here
+// function multiply() {
+//   return num1 * num2;
+// }
+
+
+let multiply = num1 * num2
+let random = Math.floor(Math.random() * 100) + 1;
+ let num3 =16
+ let num4 =4
+let divider = num3 / num4;
+let mod = 16 % 6;
+let max = Math.max(1, 5, 10, 20);
+console.log(multiply);
+console.log(random);
+console.log(divider);
+console.log(mod);
+console.log(max);
